5209a9dcd623a2ab309bc5b7e4688f5779447b30
AvianMaiden <188556051+AvianMaiden@users.noreply.github.com>
2025-12-16 18:23:46 -0800

    OOPS

--- Content.Server/_Lavaland/Weather/Gamerule/LavalandStormSchedulerRuleComponent.cs
+++ Content.Server/_Lavaland/Weather/Gamerule/LavalandStormSchedulerRuleComponent.cs
@@ -13,10 +13,10 @@ public sealed partial class LavalandStormSchedulerRuleComponent : Component
     /// <summary>
     ///     How long until the next check for an event runs
     /// </summary>
-    [DataField] public float EventClock = 1f; // Ten minutes
+    [DataField] public float EventClock = 600f; // Ten minutes
 
     /// <summary>
     ///     How much time it takes in seconds for a lavaland storm to be raised.
     /// </summary>
-    [DataField] public MinMax Delays = new(20 * 6, 40 * 6);
+    [DataField] public MinMax Delays = new(20 * 60, 40 * 60);
 }
